255f4976b6774356b80bf2b717922ec18d9f64ec
zzh <10003482+zengzhaohui@user.noreply.gitee.com>
2023-09-07 15:47:05 +0800

    aside css 1

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -25,9 +25,25 @@
 </template>
 
 <style lang="scss" scoped>
-
+  aside{
+    background: lightblue;
+    width: 150px;
+    padding: 16px;
+    position: fixed;
+    top: 0;
+    left: 0;
+    >h2 {
+      margin-bottom: 4px;
+    }
+    >ol {
+      >li {
+        padding: 4px 0;
+      }
+    }
+  }
 </style>
-<script setup>
+
+<script lang="ts">
 import Topnav from '../components/Topnav.vue';
 export default {
   components: {Topnav}
