37fdbccaf3102320ad229343b7993716b64d60a5
zzh <10003482+zengzhaohui@user.noreply.gitee.com>
2023-09-09 11:49:24 +0800

    Dialog 支持 visible

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,14 +1,24 @@
 <template>
   <div>Dialog示例</div>
   <h1>示例1</h1>
-  <Dialog />
+  <Button @click="toggle">toggle</Button>
+  <Dialog v-model:visible="visible"/>
 </template>
 
 <script lang="ts">
 import Dialog from '../lib/Dialog.vue';
+import Button from '../lib/Button.vue';
+import {ref} from 'vue';
 
 export default {
-  components: {Dialog}
+  components: {Button, Dialog},
+  setup() {
+    const visible = ref(false)
+    const toggle = () => {
+      visible.value = !visible.value
+    }
+    return {visible, toggle}
+  }
 };
 </script>
 
