9ce0f0a84d72e257fd8fbf2458343ecd7b53a14a
zzh <10003482+zengzhaohui@user.noreply.gitee.com>
2023-09-09 12:00:53 +0800

    Dialog 支持点击关闭

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -2,7 +2,7 @@
   <div>Dialog示例</div>
   <h1>示例1</h1>
   <Button @click="toggle">toggle</Button>
-  <Dialog v-model:visible="visible"/>
+  <Dialog v-model:visible="visible" :closeOnClickOverlay="false" :ok="f1" :cancel="f2"/>
 </template>
 
 <script lang="ts">
@@ -17,7 +17,13 @@ export default {
     const toggle = () => {
       visible.value = !visible.value
     }
-    return {visible, toggle}
+    const f1 = () => {
+      return false
+    }
+    const f2 = () => {
+
+    }
+    return {visible, toggle, f1, f2}
   }
 };
 </script>
