1f719c63962958d1baf1c724a637722eea86ce3c
zzh <10003482+zengzhaohui@user.noreply.gitee.com>
2023-09-10 20:54:22 +0800

    使用 title 切换内容

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -2,19 +2,30 @@
   <div>
     <div class="gulu-tabs">
       <div class="gulu-tabs-nav">
-        <div class="gulu-tabs-nav-item" v-for="(t, index) in titles" :key="index">{{t}}</div>
+        <div class="gulu-tabs-nav-item"
+             v-for="(t, index) in titles"
+             @click="select(t)"
+             :class="{selected: t===selected}"
+             :key="index">{{t}}
+        </div>
       </div>
     </div>
     <div class="gulu-tabs-content">
-      <component v-for="(c, index) in defaults" :is="c" :key="index" />
+      <component class="gulu-tabs-content-item" :is="current" :key="selected" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import Tab from './Tab.vue';
+import {computed} from 'vue';
 
 export default {
+  props: {
+    selected: {
+      type: String
+    }
+  },
   setup(props, context){
     const defaults = context.slots.default()
     defaults.forEach((tag) => {
@@ -25,7 +36,15 @@ export default {
     const titles = defaults.map((tag) => {
       return tag.props.title
     })
-    return {defaults, titles}
+    const current = computed(() => {
+      return defaults.filter((tag) => {
+        return tag.props.title === props.selected
+      })[0]
+    })
+    const select = (title: string) => {
+      context.emit('update:selected', title)
+    }
+    return {defaults, titles, select, current}
   }
 };
 </script>
