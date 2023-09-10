6bcc0f4cafe3fd3e7a0aae424c0eeb0cf6c85ef3
zzh <10003482+zengzhaohui@user.noreply.gitee.com>
2023-09-10 22:18:52 +0800

    实现下划线滑动

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
     <div class="gulu-tabs">
-      <div class="gulu-tabs-nav">
+      <div class="gulu-tabs-nav" ref="container">
         <div class="gulu-tabs-nav-item"
              v-for="(t, index) in titles"
+             :ref="el => { if (el) navItems[index] = el }"
              @click="select(t)"
              :class="{selected: t===selected}"
              :key="index">{{t}}
         </div>
-        <div class="gulu-tabs-nav-indicator"></div>
+        <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
       </div>
     </div>
     <div class="gulu-tabs-content">
@@ -19,7 +20,7 @@
 
 <script lang="ts">
 import Tab from './Tab.vue';
-import {computed} from 'vue';
+import {computed, onMounted, onUpdated, ref} from 'vue';
 
 export default {
   props: {
@@ -28,6 +29,21 @@ export default {
     }
   },
   setup(props, context){
+    const navItems = ref<HTMLDivElement[]>([])
+    const indicator = ref<HTMLDivElement>(null)
+    const container = ref<HTMLDivElement>(null)
+    const x = () => {
+      const divs = navItems.value
+      const result = divs.filter(div => div.classList.contains('selected'))[0]
+      const {width} = result.getBoundingClientRect()
+      indicator.value.style.width = width + 'px'
+      const {left: left1} = container.value.getBoundingClientRect()
+      const {left: left2} = result.getBoundingClientRect()
+      const left = left2 - left1
+      indicator.value.style.left = left + 'px'
+    }
+    onMounted(x)
+    onUpdated(x)
     const defaults = context.slots.default()
     defaults.forEach((tag) => {
       if(tag.type !== Tab ) {
@@ -45,7 +61,7 @@ export default {
     const select = (title: string) => {
       context.emit('update:selected', title)
     }
-    return {defaults, titles, select, current}
+    return {defaults, titles, select, current, navItems, indicator, container}
   }
 };
 </script>
@@ -77,6 +93,7 @@ export default {
         height: 3px;
         background: $blue;
         left: 0;
+        transition: all 250ms;
         bottom: -1px;
         width: 100px;
       }
