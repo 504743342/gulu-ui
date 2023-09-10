7e98c64569c40c04e1b871bb850d7365d8c3af39
zzh <10003482+zengzhaohui@user.noreply.gitee.com>
2023-09-10 22:27:52 +0800

    把navItem 优化成 selectedItem

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -4,7 +4,7 @@
       <div class="gulu-tabs-nav" ref="container">
         <div class="gulu-tabs-nav-item"
              v-for="(t, index) in titles"
-             :ref="el => { if (el) navItems[index] = el }"
+             :ref="el => { if (t===selected) selectedItem = el }"
              @click="select(t)"
              :class="{selected: t===selected}"
              :key="index">{{t}}
@@ -29,16 +29,15 @@ export default {
     }
   },
   setup(props, context){
-    const navItems = ref<HTMLDivElement[]>([])
+    const selectedItem = ref<HTMLDivElement>(null)
     const indicator = ref<HTMLDivElement>(null)
     const container = ref<HTMLDivElement>(null)
     const x = () => {
-      const divs = navItems.value
-      const result = divs.filter(div => div.classList.contains('selected'))[0]
-      const {width} = result.getBoundingClientRect()
+      console.log(selectedItem.value);
+      const {width} = selectedItem.value.getBoundingClientRect()
       indicator.value.style.width = width + 'px'
       const {left: left1} = container.value.getBoundingClientRect()
-      const {left: left2} = result.getBoundingClientRect()
+      const {left: left2} = selectedItem.value.getBoundingClientRect()
       const left = left2 - left1
       indicator.value.style.left = left + 'px'
     }
@@ -61,7 +60,7 @@ export default {
     const select = (title: string) => {
       context.emit('update:selected', title)
     }
-    return {defaults, titles, select, current, navItems, indicator, container}
+    return {defaults, titles, select, current, selectedItem, indicator, container}
   }
 };
 </script>
