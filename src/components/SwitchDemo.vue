8cdec9ce14b67ae53c2bc1addd6311512e22df60
zzh <10003482+zengzhaohui@user.noreply.gitee.com>
2023-09-12 22:54:39 +0800

    删除无用代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -6,50 +6,18 @@
   </div>
 </template>
 <script lang="ts">
-import {ref} from 'vue';
-import Button from '../lib/Button.vue';
 import Switch1Demo from './Switch1.demo.vue';
 import Switch2Demo from './Switch2.demo.vue';
 import Demo from './Demo.vue';
-import 'prismjs';
-const Prism = (window as any).Prism
+
 export default {
-  components: { Button, Demo },
+  components: { Demo },
   setup() {
-    const bool = ref(false)
-    return {bool, Switch1Demo, Switch2Demo, Prism}
+    return {Switch1Demo, Switch2Demo}
   }
 };
 </script>
 
-<style lang="scss">
-  @import 'prismjs/themes/prism.css';
-</style>
 <style lang="scss" scoped>
-  $border-color: #d9d9d9;
-  .demo {
-    border: 1px solid $border-color;
-    margin: 16px 0 32px;
-    >h2 {
-      font-size: 20px;
-      padding: 8px 16px;
-      border-bottom: 1px solid $border-color;
-    }
-    &-component {
-      padding: 16px;
-    }
-    &-actions {
-      padding: 8px 16px;
-      border-top: 1px dashed $border-color;
-    }
-    &-code {
-      padding: 8px 16px;
-      border-top: 1px dashed $border-color;
-      >pre {
-        line-height: 1.1;
-        font-family: Consolas, 'Courier New', Courier, monospace;
-        margin: 0;
-      }
-    }
-  }
+ 
 </style>
