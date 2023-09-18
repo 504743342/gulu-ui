a290adefd8ffd54c1fc375db4ef535a4ad272fb3
zzh <10003482+zengzhaohui@user.noreply.gitee.com>
2023-09-18 21:41:33 +0800

    setup

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ git init &&
 git add . &&
 git commit -m "update" &&
 git branch -M main &&
-git remote add origin git@github.com:504743342/king-ui-website.git &&
-git push -f -u origin master &&
+git remote add origin git@github.com:zengzhui/king-ui-website.git &&
+git push -f -u origin main &&
 cd -
+echo https://zengzhui.github.io/king-ui-website/
